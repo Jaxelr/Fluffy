41d456495cfb0e04d39b47e0c7ba058b59b3c280
Jaxel Rojas <jaxelr@gmail.com>
2021-08-26 16:30:10 -0400

    refactor: remove unused code

--- Fluffy/FlufExtensions.cs
+++ Fluffy/FlufExtensions.cs
@@ -5,18 +5,5 @@ namespace Fluffy
     public static class FlufExtensions
     {
         public static bool ApplyRule<T>(this T poco, Func<T, bool> func) => func(poco);
-
-        public static (bool validation, string? error) Resolve<T>(this T poco)
-        {
-            foreach ((var rule, string error) in rules)
-            {
-                if (!rule(poco)
-                {
-                    return (false, error);
-                }
-            }
-
-            return (true, null);
-        }
     }
 }
