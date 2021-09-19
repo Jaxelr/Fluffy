f31a7065bf2f25a8717cbd64e4d1fddee44155a0
Jaxel Rojas <jaxelr@gmail.com>
2021-09-19 08:27:00 -0400

    feat: added example prj

---
+++ example/Program.cs
@@ -0,0 +1,44 @@
+﻿using Fluffy;
+using System;
+
+namespace Sample.Usage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var poco = new Poco() { Id = 2, Name = "User not" };
+            var enforcer = new PocoValidator();
+
+            var result = enforcer.Resolve(poco);
+
+            var gimme = poco.ApplyRule(d => d.Name == "Pandy");
+
+
+            if (!result.validation)
+            {
+                foreach (var error in result.errors)
+                    Console.WriteLine(error);
+            }
+
+            Console.Write("Done");
+
+            Console.Read();
+        }
+    }
+
+    public class PocoValidator : Fluf<Poco>
+    {
+        public PocoValidator()
+        {
+            Define(x => x.Id == 1, "Id does not match 1");
+            Define(x => x.Name == "User", "User is not user");
+        }
+    }
+
+    public record Poco
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
